75c01cbd6b8b8bfc762f431daedb0ba6c156389c
John Children <john.a.children@gmail.com>
2018-08-12 16:36:51 +0100

    Adds new iterator types

--- int64iterator/int64iterator_iterator.go
+++ int64iterator/int64iterator_iterator.go
@@ -34,7 +34,7 @@ func (it *Iterator) Last() Maybe {
 
 func (it *Iterator) Nth(n int) Maybe {
 	var nth Maybe = None{}
-	for v := 0; v <= n; v++ {
+	for i := 0; i <= n; i++ {
 		nth = it.Next()
 	}
 	return nth
@@ -48,6 +48,58 @@ func (it *Iterator) ForEach(f func(x int64)) {
 		case None:
 			return
 		}
+	}
+}
+
+func (it *Iterator) Chain(other *Iterator) *Iterator {
+	return &Iterator{&chainIterator{front: it, back: other, state: chainBoth}}
+}
+
+type stepByIterator struct {
+	internal *Iterator
+	step     int
+}
+
+func (s *stepByIterator) Next() Maybe {
+	next := s.internal.Next()
+	for i := 0; i <= s.step; i++ {
+		next = s.internal.Next()
+	}
+	return next
+}
+
+type chainIterator struct {
+	front *Iterator
+	back  *Iterator
+	state chainState
+}
+
+type chainState int
+
+const (
+	chainBoth chainState = iota
+	chainFront
+	chainBack
+)
+
+func (c *chainIterator) Next() Maybe {
+	switch c.state {
+	case chainBoth:
+		switch mt := c.Next().(type) {
+		case Just:
+			return mt
+		case None:
+			c.state = chainBack
+			return c.back.Next()
+		default:
+			panic("invalid maybe type")
+		}
+	case chainFront:
+		return c.front.Next()
+	case chainBack:
+		return c.back.Next()
+	default:
+		panic("invalid chain state")
 	}
 }
 
